c599628b208f45ddb01af04a3beaf9b7c73825db
goodeecomputer <viostarryy@gmail.com>
2022-07-15 14:04:02 +0900

    1

---
+++ src/com/iu/terran/Race.java
@@ -0,0 +1,11 @@
+package com.iu.terran;
+
+import com.iu.unit.AirUnit;
+
+public class Race extends AirUnit{
+	//비행기
+	public void move() {
+		
+	}
+
+}
---
+++ src/com/iu/zerg/Hydra.java
@@ -0,0 +1,21 @@
+package com.iu.zerg;
+
+import com.iu.unit.AirUnit;
+
+public class Hydra extends AirUnit {
+	
+	//hp, color, name
+	//움직이고, 공격 침뱉기
+	
+	public void move() {
+		System.out.println("꾸물꾸물");
+	}
+	
+	public void attack() {
+		System.out.println("퉤");
+	}
+	
+
+	
+
+}
